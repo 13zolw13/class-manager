def617ba67b4caecb0f960e1cd4af419681569ea
tomek <tomaszpawelkowalski@gmail.com>
2022-04-25 15:12:17 +0200

    set up typeorm

--- src/modules/database/database.module.ts
+++ src/modules/database/database.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { DatabaseService } from './database.service';
-import { DatabaseController } from './database.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  controllers: [DatabaseController],
-  providers: [DatabaseService],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: DatabaseService,
+    }),
+  ],
 })
 export class DatabaseModule {}
--- src/modules/database/database.service.ts
+++ src/modules/database/database.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService implements TypeOrmOptionsFactory {
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'classmanager',
+      // entities: [__dirname + '/**/*.entity{.ts,.js}'],//
+      entities: [User],
+      synchronize: false,
+    };
+  }
+}
