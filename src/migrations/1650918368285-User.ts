d511e137da02d99d427154304fed14b3825351cf
tomek <tomaszpawelkowalski@gmail.com>
2022-04-27 17:02:36 +0200

    clean up. unused migrations.

--- src/migrations/1650918368285-User.ts
+++
@@ -1,7 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class User1650918368285 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
