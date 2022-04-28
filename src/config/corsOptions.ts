1d646ff3c2e70ab956c34c7d66e91f84a984f9ac
tomek <tomaszpawelkowalski@gmail.com>
2022-04-28 21:49:30 +0200

    refactor: cors config

---
+++ src/config/corsOptions.ts
@@ -0,0 +1,6 @@
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+export const corsOptions: CorsOptions = {
+  origin: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
+import { corsOptions } from './config/corsOptions';
 import { SwaggerConfig } from './config/swagger';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors(corsOptions);
 
   const document = SwaggerModule.createDocument(app, SwaggerConfig);
   SwaggerModule.setup('api', app, document);
