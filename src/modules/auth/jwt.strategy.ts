43bb2883680f71b2ad1f6d48edf9e778fb9d9003
tomek <tomaszpawelkowalski@gmail.com>
2022-05-11 17:54:50 +0200

    refactor: remove console.log

--- src/modules/auth/jwt.strategy.ts
+++ src/modules/auth/jwt.strategy.ts
@@ -16,7 +16,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    console.log('validate payload', payload);
     return {
       userId: payload.sub,
       username: payload.username,
