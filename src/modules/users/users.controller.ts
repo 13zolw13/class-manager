31554f4f1dfadf4b8231d0e33488a9285feb9140
tomek <tomaszpawelkowalski@gmail.com>
2022-05-12 12:30:27 +0200

    feat: add authorization on update user endpoint

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -6,12 +6,16 @@ import {
   Param,
   Patch,
   Post,
+  Request,
   UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { STATUS_CODES } from 'http';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { Action } from '../casl/actions';
+import { CaslAbilityFactory } from '../casl/casl-ability.factory';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
@@ -19,7 +23,10 @@ import { UsersService } from './users.service';
 @UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private caslAbilityFactory: CaslAbilityFactory,
+  ) {}
 
   @Post()
   @UsePipes(new ValidationPipe({ transform: true }))
@@ -38,8 +45,20 @@ export class UsersController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+    @Request() req,
+  ) {
+    const ability = this.caslAbilityFactory.createForUser(req.user);
+    console.log(req.user);
+    console.log(ability.can(Action.Manage, 'all'));
+    return ability.can(Action.Manage, 'all')
+      ? this.usersService.update(+id, updateUserDto)
+      : {
+          statusCode: STATUS_CODES.UNAUTHORIZED,
+          message: 'You are not authorized to perform this action',
+        };
   }
 
   @Delete(':id')
--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CaslModule } from '../casl/casl.module';
 import { ContactInformation } from './entities/contactInfo.entity';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, ContactInformation])],
+  imports: [TypeOrmModule.forFeature([User, ContactInformation]), CaslModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
