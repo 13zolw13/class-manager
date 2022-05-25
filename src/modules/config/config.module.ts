6df5e45923a7eab3a580ab910752cf1b4f4ce2fc
tomek <tomaszpawelkowalski@gmail.com>
2022-05-25 22:53:55 +0200

    feat: load db configuration file

--- src/modules/config/config.module.ts
+++ src/modules/config/config.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import databaseConfig from '../../config/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ['.env'],
+      envFilePath: [
+        '.env',
+        process.env.NODE_ENV === 'TEST' ? '.env.test' : '.env.dev',
+      ],
       cache: true,
+      load: [databaseConfig],
     }),
   ],
 })
