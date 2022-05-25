389faf053789d20fc027458dc4c65243dccbf5a9
tomek <tomaszpawelkowalski@gmail.com>
2022-05-25 15:11:13 +0200

    refactor: set up configuration module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from '../auth/auth.module';
 import { CaslModule } from '../casl/casl.module';
+import { ConfigurationModule } from '../config/config.module';
 import { DatabaseModule } from '../database/database.module';
 import { UsersModule } from '../users/users.module';
 import { AppController } from './app.controller';
@@ -9,11 +9,7 @@ import { AppService } from './app.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: ['../../.env'],
-      cache: true,
-    }),
+    ConfigurationModule,
     UsersModule,
     DatabaseModule,
     AuthModule,
